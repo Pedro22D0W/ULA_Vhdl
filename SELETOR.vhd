a27f9bf1bf7afc97e160096c2611c5e28c3f0a8c
Pedro Rodolfo Silva Galvão Santos <110983216+Pedro22D0W@users.noreply.github.com>
2024-05-06 16:54:41 -0300

    Add files via upload

--- SELETOR.vhd
+++ SELETOR.vhd
@@ -11,20 +11,49 @@ ENTITY SELETOR IS
 END SELETOR;
 
 ARCHITECTURE LOGIC OF SELETOR IS
+SIGNAL E2 : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL OU2 : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL NAO2 : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL ADD : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL SUB : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL COMPARA2 : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL MAIOR2 : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL MENOR2: STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+
 BEGIN 
-    PROCESS(W, X, Y)
+
+
+
+s0: E PORT MAP( X,Y,E2 );
+s1: OU PORT MAP( X,Y,OU2 );
+s2: NAO PORT MAP( Y,NAO2 );
+s3: ADD_SUB PORT MAP( '0',X,Y,ADD );
+s4: ADD_SUB PORT MAP( '1',X,Y,SUB );
+s5: COMPARA PORT MAP( X,Y,COMPARA2 );
+s6: MAIOR PORT MAP( X,Y,MAIOR2 );
+s7: MENOR PORT MAP( X,Y,MENOR2 );
+	 
+    PROCESS(W)
     BEGIN 
+	 
         CASE W IS 
             WHEN "000" =>
-                S <= E(X, Y );
+                S <= E2;
             WHEN "001" =>
-                S <= OU(X, Y);
+                S <= OU2;
             WHEN "010" =>
-                S <= NAO(Y);
+                S <= NAO2;
             WHEN "011" =>
-                S <= ADD_SUB('0', X, Y); -- '0' para soma e '1' para subtração
+                S <= ADD; 
+				WHEN "100" =>
+                S <= SUB; 
 				 WHEN "101" =>
-                S <= COMPARA(X,Y); -- compara se é igual, maior ou menor 
+                S <= COMPARA2;
+				WHEN "110" =>
+                S <= MAIOR2;
+				WHEN "111" =>
+                S <= MENOR2; 	 
             WHEN OTHERS =>
                 NULL;
         END CASE;
